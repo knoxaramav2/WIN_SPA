f5870adad82e37e2197df95b4307084ffbc2babf
Woodrow <wtscott@iastate.edu>
2017-04-18 18:44:55 -0500

    VS

---
+++ Core/Core/Launcher.cpp
@@ -0,0 +1,19 @@
+#include <string>
+
+int parseCmd(int argc, char ** argv)
+{
+	int ret = 0;
+
+	
+	return ret;
+}
+
+int main(int argc, char ** argv)
+{
+	int rcode = parseCmd(argc, argv);
+	if (rcode != 0) return rcode;
+
+
+
+	return 0;
+}
